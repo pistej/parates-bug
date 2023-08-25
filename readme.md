2901d3972cba1ece2e7674bff938f28139950e94
Jozef Pistej <jozef.pistej@websupport.sk>
2023-08-25 17:07:02 +0200

    paratest junit bug example

---
+++ readme.md
@@ -0,0 +1,5 @@
+
+ParaTest v7.2.5 upon PHPUnit 10.3.2
+PHP 8.1.22
+
+Command to run tests: ` ./vendor/bin/paratest --processes=1 --functional --max-batch-size=1 -v tests`
---
+++ tests/Unit/OneTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace tests\Unit;
+
+class OneTest extends \PHPUnit\Framework\TestCase
+{
+    public function testOne(){
+        $this->assertTrue(true);
+    }
+
+    public function testTwo(){
+        $this->assertTrue(true);
+    }
+//
+//    public function testThree(){
+//        $this->assertTrue(true);
+//    }
+//
+//    public function testFour(){
+//        $this->assertTrue(true);
+//    }
+}
